498484151493ae89797674873b77ceb76468c03c
h-takeda <h-takeda@jsk.imi.i.u-tokyo.ac.jp>
2018-12-14 22:21:35 +0900

    add InvertedPendulum simulation

---
+++ controllers/PDController_InvertedPendulum.cpp
@@ -0,0 +1,79 @@
+// -*- mode: C++; coding: utf-8-unix; -*-
+
+/**
+ * @file  PDcontroller_InvertedPendulum.cpp
+ * @brief
+ * @author Hiroki Takeda
+ */
+
+#include <cnoid/SimpleController>
+#include <cnoid/AccelerationSensor>
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+
+using namespace cnoid;
+
+class PDController_InvertedPendulum : public SimpleController
+{
+  Link* joint;
+  double q_prev;
+  
+  AccelerationSensor* accelSensor;
+  double theta_prev;
+
+  double dt;
+  
+public:
+  bool initialize(SimpleControllerIO* io) override
+  {
+    accelSensor = io->body()->findDevice<AccelerationSensor>("RodAccelSensor");
+    io->enableInput(accelSensor);
+    theta_prev = accelSensor->dv().x();
+
+    joint = io->body()->link("WHEEL");
+    joint->setActuationMode(Link::JOINT_TORQUE);
+    io->enableIO(joint);
+    q_prev = joint->q();
+
+    dt = io->timeStep();
+
+    return true;
+  }
+
+  bool control() override
+  {
+    static const double P_joint = 0;
+    static const double D_joint = 0;
+    static const double P_theta = 0.3;
+    static const double D_theta = 0;
+
+    static const double q_ref = 0.0;
+    static const double dq_ref = 0.0;
+    static const double theta_ref = 0.0;
+    static const double dtheta_ref = 0.0;
+
+    double q = joint->q();
+    double dq = (q - q_prev) / dt;
+    Vector3 dv = accelSensor->dv();
+    double theta = theta_ref;
+    if(dv.norm() > DBL_EPSILON)
+      theta = asin((dv / dv.norm()).x()) * 180 / 3.141592;
+    double dtheta = (theta - theta_prev) / dt;
+    joint->u() = 
+      P_joint * (q_ref - q)
+      + D_joint * (dq_ref - dq)
+      + P_theta * (theta_ref - theta)
+      + D_theta * (dtheta_ref - dtheta);
+    std::cerr << q << " " << dq << " " << theta << " " << dtheta << " " << joint->u() << std::endl;
+
+    theta_prev = theta;
+    q_prev = q;
+
+    return true;
+  }
+};
+
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(PDController_InvertedPendulum)
+
+
