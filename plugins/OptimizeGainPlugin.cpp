cfc894a5cfb8e26f8e8d18ac7f50f1ebb49a78e5
h-takeda <h-takeda@jsk.imi.i.u-tokyo.ac.jp>
2018-12-27 02:34:52 +0900

    can communicate gains from plugin to controller

--- plugins/OptimizeGainPlugin.cpp
+++ plugins/OptimizeGainPlugin.cpp
@@ -12,6 +12,10 @@
 #include <cnoid/MessageView>
 #include <cnoid/Timer>
 
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+
+using namespace boost::interprocess;
 using namespace cnoid;
 
 class OptimizeGainPlugin : public Plugin
@@ -39,8 +43,15 @@ private:
     void resetSimulation()
     {
         if (simulator_item_) {
-            if (simulator_item_->simulationTime() > reset_interval_)
+            if (simulator_item_->simulationTime() > reset_interval_) {
                 simulator_item_->startSimulation(true);
+                shared_memory_object shdmem{open_or_create, "Gain", read_write};
+                shdmem.truncate(1024);
+                mapped_region region{shdmem, read_write};
+                double *gain = static_cast<double*>(region.get_address());
+                for (int i = 0; i < 4; i++)
+                    gain[i] = 0.01 * i;
+            }
         } else {
             ItemPtr robot_item = RootItem::instance()->findItem("InvertedPendulum");
             simulator_item_ = SimulatorItem::findActiveSimulatorItemFor(robot_item);
