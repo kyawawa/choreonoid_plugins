1202bf3bfeedd570b3179dff249238003e219ab0
h-takeda <h-takeda@jsk.imi.i.u-tokyo.ac.jp>
2018-12-26 22:33:08 +0900

    simple shm test

---
+++ shm_test/publisher.cpp
@@ -0,0 +1,14 @@
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <iostream>
+
+using namespace boost::interprocess;
+
+int main()
+{
+  shared_memory_object shdmem{open_or_create, "Boost", read_write};
+  shdmem.truncate(1024);
+  mapped_region region{shdmem, read_write};
+  int *i1 = static_cast<int*>(region.get_address());
+  *i1 = 666;
+}
---
+++ shm_test/subscriber.cpp
@@ -0,0 +1,14 @@
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <iostream>
+
+using namespace boost::interprocess;
+
+int main()
+{
+  shared_memory_object shdmem{open_or_create, "Boost", read_write};
+  shdmem.truncate(1024);
+  mapped_region region2{shdmem, read_only};
+  int *i2 = static_cast<int*>(region2.get_address());
+  std::cout << *i2 << '\n';
+}
