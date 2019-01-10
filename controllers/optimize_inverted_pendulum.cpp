6c00d452afcc4b94c555da9ceacbdf2c9b0aae1f
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2019-01-10 13:23:50 +0900

    Modify cost function to maximize, add displacement from init position and output cost for each epoch

--- controllers/optimize_inverted_pendulum.cpp
+++ controllers/optimize_inverted_pendulum.cpp
@@ -21,46 +21,17 @@
 using namespace boost::interprocess;
 
 namespace {
-constexpr size_t NUM_PARAMS = 2;
-constexpr double FAIL_PENALTY = 10;
+constexpr size_t NUM_PARAMS = 3;
+constexpr double FAIL_PENALTY = 30;
 constexpr double FAIL_ANGLE = 1.2;
 struct GainWithCost {
-    std::vector<double> gains{1.0, 0.0};
+    std::vector<double> gains{1.0, 1.0, 1.0};
     double cost{0};
     int is_finished{0};
 };
 }
 
 namespace cnoid {
-
-template <class T> class FirstOrderLowPassFilter
-{
-  private:
-    T prev_value;
-    double cutoff_freq, dt, const_param;
-  public:
-    FirstOrderLowPassFilter (const double _cutoff_freq, const double _dt, const T init_value) : prev_value(init_value), dt(_dt)
-    {
-        setCutOffFreq(_cutoff_freq);
-    };
-    ~FirstOrderLowPassFilter()
-    {
-    };
-    T passFilter (const T& value)
-    {
-        prev_value = 1.0 / (1 + const_param) * prev_value + const_param / (1 + const_param) * value;
-        return prev_value;
-    };
-    void reset (const T& value) { prev_value = value; };
-    void setCutOffFreq (const double f)
-    {
-        cutoff_freq = f;
-        const_param = 2 * M_PI * cutoff_freq * dt;
-    };
-    double getCutOffFreq () const { return cutoff_freq; };
-    T getCurrentValue () const { return prev_value; };
-};
-
 
 class OptimizeInvertedPendulum : public SimpleController
 {
@@ -73,12 +44,11 @@ class OptimizeInvertedPendulum : public SimpleController
     cnoid::Vector3 gyro_prev;
     cnoid::Vector3 gyro_w;
     double gyro_acc;
-    cnoid::Matrix3 init_rod;
+    cnoid::Position init_rod;
+    // cnoid::Matrix3 init_rod_rot;
     cnoid::Vector3 rod_rot;
-    std::shared_ptr<FirstOrderLowPassFilter<cnoid::Vector3>> gyro_filter;
 
     GainWithCost opt_data;
-    double goal_time;
     unsigned int count;
     double dt;
 
@@ -93,11 +63,11 @@ class OptimizeInvertedPendulum : public SimpleController
         shm_gain = shared_memory_object{open_only, GAIN_SHM, read_write};
         mapped_region region_gain{shm_gain, read_write};
         GainWithCost* data = static_cast<GainWithCost*>(region_gain.get_address());
-        opt_data.gains[0] = data->gains[0];
-        opt_data.gains[1] = data->gains[1];
+        for (size_t i = 0; i < opt_data.gains.size(); ++i) {
+            opt_data.gains[i] = data->gains[i];
+        }
         data->is_finished = 0;
 
-        goal_time = 10.0;
         count = 0;
         dt = io->timeStep();
 
@@ -117,8 +87,7 @@ class OptimizeInvertedPendulum : public SimpleController
         gyro_prev.setZero();
         gyro_w.setZero();
         gyro_acc = 0;
-        gyro_filter = std::make_shared<FirstOrderLowPassFilter<cnoid::Vector3>>(50, dt, cnoid::Vector3::Zero());
-        init_rod = rod->rotation();
+        init_rod = rod->position();
         rod_rot.setZero();
 
         return true;
@@ -132,7 +101,6 @@ class OptimizeInvertedPendulum : public SimpleController
     bool control() override
     {
         if (opt_data.is_finished == 0) {
-            // gyro_w = gyro_filter->passFilter(gyro_sensor->w());
             gyro_w = gyro_sensor->w();
             // std::cerr << "gyro: " << gyro_sensor->w().transpose() << std::endl;
             wheel->dq() = (wheel->q() - wheel_q_prev) / dt;
@@ -150,7 +118,7 @@ class OptimizeInvertedPendulum : public SimpleController
             wheel_q_prev = wheel->q();
             gyro_prev = gyro_w;
 
-            wheel->u() = gyro_acc * opt_data.gains[0] + rod_rot.sum() * opt_data.gains[1];
+            wheel->u() = gyro_acc * opt_data.gains[0] + rod_rot.sum() * opt_data.gains[1] + (init_rod.translation() - rod->translation()).sum() * opt_data.gains[2];
             // wheel->u() = 0;
             ++count;
         }
@@ -167,7 +135,7 @@ class OptimizeInvertedPendulum : public SimpleController
         // std::cerr << Eigen::AngleAxisd(init_rod.transpose() * rod->rotation()).angle() << std::endl;
         // std::cerr << rod->rotation() << std::endl;
         // std::cerr << "acc:  " << gyro_acc << std::endl;
-        if (Eigen::AngleAxisd(init_rod.transpose() * rod->rotation()).angle() > FAIL_ANGLE || rod->rotation().array().isNaN().any()) {
+        if (Eigen::AngleAxisd(init_rod.linear().transpose() * rod->rotation()).angle() > FAIL_ANGLE || rod->rotation().array().isNaN().any()) {
             // std::cerr << "gyro: " << gyro_sensor->w().transpose() << std::endl;
             // std::cerr << "prev: " << gyro_prev.transpose() << std::endl;
             // std::cerr << "acc:  " << gyro_acc << std::endl;
@@ -180,17 +148,18 @@ class OptimizeInvertedPendulum : public SimpleController
     double calcCost() const
     {
         if (opt_data.is_finished == -1) {
-            if (rod->rotation().array().isNaN().any()) return (goal_time - count * dt) + FAIL_PENALTY + 100;
-            return (goal_time - count * dt) + FAIL_PENALTY;
-        } else return (goal_time - count * dt) + std::abs(wheel->dq()) + gyro_w.cwiseAbs().sum();
+            if (rod->rotation().array().isNaN().any()) return count * dt - FAIL_PENALTY - 100;
+            return count * dt - FAIL_PENALTY;
+        } else return count * dt - (std::abs(wheel->dq()) + gyro_w.cwiseAbs().sum() + (init_rod.translation() - rod->translation()).cwiseAbs().sum());
     }
 
     void readGain()
     {
         mapped_region region_gain{shm_gain, read_only};
         GainWithCost* data = static_cast<GainWithCost*>(region_gain.get_address());
-        opt_data.gains[0] = data->gains[0];
-        opt_data.gains[1] = data->gains[1];
+        for (size_t i = 0; i < opt_data.gains.size(); ++i) {
+            opt_data.gains[i] = data->gains[i];
+        }
     }
 
     void writeData()
